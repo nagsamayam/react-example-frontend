ff5288cdf8502753478359fa84a47fb6a74b298c
Steve Waugh <stevewaughbitrix@gmail.com>
2021-11-24 11:29:01 +0530

    Update user

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -64,11 +64,15 @@ const Users = () => {
                                     <td>{user.roles.map((role)=> role.name)}</td>
                                     <td>
                                         <div className="btn-group mr-2">
+                                            <Link to={`/users/${user.id}/edit`} className="btn btn-sm btn-outline-secondary">
+                                                Edit
+                                            </Link>
                                             <a 
                                                 href="#" 
                                                 className="btn btn-sm btn-outline-secondary"
-                                                onClick={(e) => del(user.id)}
-                                            >Delete</a>
+                                                onClick={(e) => del(user.id)}>
+                                                Delete
+                                            </a>                                            
                                         </div>
                                     </td>
                                 </tr>                                
