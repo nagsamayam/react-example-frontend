b186397b7eedd213ba41594344a1828973091d9e
Steve Waugh <stevewaughbitrix@gmail.com>
2021-11-24 09:33:03 +0530

    Create user

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Wrapper from "../../componets/Wrapper";
 
 const Users = () => {
@@ -39,6 +40,9 @@ const Users = () => {
 
     return(
         <Wrapper>
+            <div className="pt-3 pb-2 mb-3 border-bottom">
+                <Link to={'/users/create'} className="btn btn-sm btn-outline-secondary">Add</Link>
+            </div>
             <div className="table-responsive">
                 <table className="table table-striped table-sm">
                     <thead>
